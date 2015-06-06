96558435e7ed980a432c69481eee794eb311b080
Wellington Viana Lobato Junior <weviloju@hotmail.com>
2015-06-06 08:25:46 -0300

    Update checador.py
    Fiz algumas alterações, pretendo contribuir mais. A questão do if e elif, eu não tava conseguindo chegar nas outras condições, se eu colocasse uma quantidade maior que 15 caracteres, ele iria entrar na primeira condição, mas esquecer de verificar se só tem numeros.
    
    Agora ele checa se só tem letras também.

--- src/checador.py
+++ src/checador.py
@@ -5,13 +5,25 @@ __author__ = 'Jose Augusto'
 Script para checar se sua senha é segura! script em constate evolução! Contribua!
 '''
 
-senha = input('Digite a senha a ser testada: ')
+#Função para checar a senha digitada pelo usuario
+def Checador(senha):
+    if len(senha) < 15:
+        print("Senha menor que 15 caracteres! não é segura!")
+        
+    elif len(senha) > 15:
+        print("Maior que 15 Senha boa!")
 
-if len(senha) < 15:
-    print("Senha menor que 15 caracteres! não é segura!")
+    if senha.isdigit(): #Estava com elif, mas não seria possivel alcançar essa condição(Ou ela é menor que 15, ou maior que quinze, ou tem só digito). Se colocar um if, ele verifica o tamanho e se a string é composta só por numeros 
+        print("Sua senha contem apenas numeros! não é segura!")
+        
+    elif senha.isalpha(): #Verifica se só tem letras na senha
+        print("Sua senha contem apenas letras! não é segura!")
+        
+    else:
+        print("Sua senha contem letras e numeros! Senha boa")
+        
 
-elif len(senha) > 15:
-    print("Maior que 15 Senha boa!")
+if __name__ == "__main__":
+    senha = input('Digite a senha a ser testada: ')
+    Checador(senha)
 
-elif senha.isdigit():
-    print("Sua senha contem apenas numeros! não é segura!")
